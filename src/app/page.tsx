a67d636de389f074d8d3858818e7f3d19a95a2e9
Shitanshu Kumar <shitanshu.kumar@impactanalytics.co>
2026-01-22 14:59:41 +0530

    03: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const projects = useQuery(api.projects.get);
+  console.log("Projects", projects)
+  const createProject = useMutation(api.projects.create)
+
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="flex flex-col gap-2 p-4">
+        <p>Hello</p>
+        <Button onClick={() => createProject({
+          name: "Project 2"
+        })}>
+          Add projects
+        </Button>
+    </main>
   );
 }
