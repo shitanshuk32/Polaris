3661b52943e1729ba4734ddc9cb2859e56c55dc4
Shitanshu Kumar <shitanshu.kumar@impactanalytics.co>
2026-02-27 15:46:56 +0530

    04: backgrond jobs

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,40 +1,43 @@
 "use client"
 
-import { 
-  Authenticated, 
-  Unauthenticated,
-  ConvexReactClient,
-  AuthLoading, 
+import {
+    Authenticated,
+    Unauthenticated,
+    ConvexReactClient,
+    AuthLoading,
 } from "convex/react";
 import { ClerkProvider, useAuth, UserButton } from "@clerk/nextjs"
-import { ConvexProviderWithClerk} from "convex/react-clerk"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
 import { ThemeProvider } from "./theme-provider";
 import { UnauthenticatedView } from "../features/components/auth/unauthenticated-view";
 import { AuthLoadingView } from "../features/components/auth/auth-loading-view";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
-export const Providers  = ({children} : {children: React.ReactNode }) => {
+export const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <ClerkProvider>
-            <ConvexProviderWithClerk client={convex} useAuth = {useAuth}>
-                <ThemeProvider          
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Authenticated>
-            <UserButton />
-            {children}
-          </Authenticated>
-          <Unauthenticated>
-            <UnauthenticatedView />
-          </Unauthenticated>
-          <AuthLoading>
-            <AuthLoadingView />
-          </AuthLoading>
-        </ThemeProvider>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Authenticated>
+                        <UserButton />
+                        {children}
+                    </Authenticated>
+
+                    <Unauthenticated>
+                        <UnauthenticatedView />
+                    </Unauthenticated>
+
+                    <AuthLoading>
+                        <AuthLoadingView />
+                    </AuthLoading>
+
+                </ThemeProvider>
             </ConvexProviderWithClerk>
         </ClerkProvider>
 
